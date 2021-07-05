7dd7030094e5f23421f150b05186c0772cb16c72
Vladimir Ivanov <omhetehmo@yandex.ru>
2021-07-05 22:42:00 +0300

    add social test channel

--- src/social/tg.ts
+++ src/social/tg.ts
@@ -1,11 +1,13 @@
 import { Telegraf } from 'telegraf';
 import config from '../config';
 
+const channelId = config.isDev ? config.SOCIAL_TG_TEST_CHANNEL_ID! : config.SOCIAL_TG_CHANNEL_ID!;
+
 export const sendToTelegram = async (imageBuffer: Buffer, text: string) => {
     console.log('Sending to Telegram');
     const bot = new Telegraf(config.SOCIAL_TG_BOT_TOKEN!);
     await bot.launch();
-    await bot.telegram.sendPhoto(config.SOCIAL_TG_CHANNEL_ID!, { source: imageBuffer }, { caption: text });
+    await bot.telegram.sendPhoto(channelId, { source: imageBuffer }, { caption: text });
     await bot.stop();
     console.log('Sending to Telegram: Done');
 };
